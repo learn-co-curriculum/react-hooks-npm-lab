5f87ec39686869b4e4735eda96e8b57881118949
Maxwell Benton <maxwellbenton@gmail.com>
2019-04-12 10:01:03 -0400

    success -> successfully
    closes #3

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ const expect = chai.expect;
 
 describe('index.js', function () {
   describe('displayTime()', function () {
-    it('can successful access moment() from the momentjs node module', function () {
+    it('can successfully access moment() from the momentjs node module', function () {
       expect(moment()).to.exist
     });
   });
