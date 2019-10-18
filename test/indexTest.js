46868020832e2b737cf261d79c7438361411b478
maxwellbenton <maxwellbenton@gmail.com>
2019-10-17 23:31:37 -0400

    moves code-along files to sub-folder

--- test/indexTest.js
+++
@@ -1,9 +0,0 @@
-const expect = chai.expect;
-
-describe('index.js', function () {
-  describe('displayTime()', function () {
-    it('can successfully access moment() from the momentjs node module', function () {
-      expect(moment()).to.exist
-    });
-  });
-});
