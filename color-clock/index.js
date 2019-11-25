7a3e7d15e6a94d33b0039f24750e1b48a9afc7a0
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-11-25 10:17:10 -0500

    Update index.js

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,4 +1,5 @@
-//No need to do any work here.  If package.json and the proper modules are installed, this will
+//No need to do any work here.  
+//If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
 
 function displayTime() {
   let clock = document.querySelector('#clock')
